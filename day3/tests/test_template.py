8ba0f2cb37c550ed39e82b0cc1bd9ee66fb7d764
amr1ta <amrita.mahapatra11@gmail.com>
2019-08-24 22:58:10 +0530

    simplify day4

--- day3/tests/test_template.py
+++ day3/tests/test_template.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from pages.base_page import BasePage
 
+
 class TestTemplate(unittest.TestCase):
     def setUp(self):
         self._driver = BasePage(webdriver.Chrome(
--- day4/tests/test_template.py
+++ day4/tests/test_template.py
@@ -1,14 +1,14 @@
 import unittest
-import HtmlTestRunner
-from pages.page_factory import get_browser_context
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from pages.base_page import BasePage
+
 
 class TestTemplate(unittest.TestCase):
     def setUp(self):
-        self._driver = get_browser_context('chrome')
+        self._driver = BasePage(webdriver.Chrome(
+            ChromeDriverManager().install()))
         self._driver.open("https://en.wikipedia.org/wiki/Main_Page")
 
     def tearDown(self):
         self._driver.quit()
-
-
-
