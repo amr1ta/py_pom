fcb19615b5064104bc96eb57b0fc38a1140b810a
amr1ta <amrita.mahapatra11@gmail.com>
2019-08-24 21:03:51 +0530

    minor

--- Day3/tests/test_template.py
+++ Day3/tests/test_template.py
@@ -3,8 +3,7 @@ from pages.page_factory import get_browser_context
 
 class TestTemplate(unittest.TestCase):
     def setUp(self):
-        context_driver = get_browser_context('chrome')
-        self._driver = context_driver
+        self._driver = get_browser_context('chrome')
         self._driver.open("https://en.wikipedia.org/wiki/Main_Page")
 
     def tearDown(self):
