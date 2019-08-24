eeb965da5e47be9ec788e3cf94f36969bcc2663a
amr1ta <amrita.mahapatra11@gmail.com>
2019-08-24 22:46:54 +0530

    trying to rename

--- Day1/pages/base_page.py
+++
@@ -1,5 +0,0 @@
-# Base Page
-
-class BasePage():
-    def __init__(self, driver):
-        self._driver = driver
--- Day3/pages/locators.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class HomePageLocators():
-    SEARCH_CONTAINER = (By.ID, "searchInput")
-    SEARCH_BUTTON = (By.ID, "searchButton")
-
-class SearchPageLocators():
-    HEADING = (By.ID, "firstHeading")
